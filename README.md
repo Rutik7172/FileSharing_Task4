09e531246970cc96e266c0193f0a681e933a863a
Rutik7172 <160102621+Rutik7172@users.noreply.github.com>
2024-03-30 22:09:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# FileSharing_Task4
+# Storage
+
+One interface to connect many of database, This package is aimed not only used for codenoid/file.io
+
+## Installation
+
+`go get -u github.com/codenoid/file.io/storage`
+
+## Storage Support
+
+- [x] Redis
+- [ ] MongoDB
+- [ ] MySQL
+- [ ] PostgreSQL
+- [ ] Cloud Storage (Google)
+- [ ] File
+
+## Example Usage
+
+```go
+
+import "github.com/codenoid/file.io/storage"
+
+// type://user:pass@host:port/database
+stg, err := storage.Connect("redis://127.0.0.1:6379/0")
+
+// then you can call this function
+stg.Set()
+stg.Get()
+Stg.Del()
+```
---
+++ redis.go
@@ -0,0 +1,42 @@
+package storage
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/go-redis/redis/v7"
+)
+
+// Redis storage
+type Redis struct {
+	Conn *redis.Client
+}
+
+// Connect will start redis connection
+func (r *Redis) Connect(addr, password, database string) {
+	intf, err := strconv.Atoi(database)
+	if err != nil {
+		panic(err)
+	}
+	r.Conn = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password, // no password set
+		DB:       intf,     // use default DB
+	})
+}
+
+// Set set bytes file to redis using unique id as key
+func (r *Redis) Set(key string, value []byte, expired time.Duration) error {
+	return r.Conn.Set(key, value, expired).Err()
+}
+
+// Get get bytes from redis and write bytes as response (file)
+func (r *Redis) Get(key string) ([]byte, error) {
+	d := r.Conn.Get(key)
+	return d.Bytes()
+}
+
+// Del uwu
+func (r *Redis) Del(key string) {
+	r.Conn.Del(key)
+}
